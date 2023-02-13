d778f4e547414b707e9ff74820ad76f995fc2a3f
Pulkit Verma <pulkit@salesken.ai>
2023-02-13 22:22:57 +0530

    initial commit: dumped all data in json for milvus

---
+++ src/main/java/com/salesken_scrapper/mavenproject/Tokenizer.java
@@ -0,0 +1,106 @@
+package com.salesken_scrapper.mavenproject;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import org.json.simple.JSONArray;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.gargoylesoftware.htmlunit.javascript.host.file.FileReader;
+import com.google.common.io.Files;
+import com.salesken_scrapper.tokenizer.GPT2Tokenizer;
+
+
+public class Tokenizer {
+	
+	static Integer maxTokenSize = 500;
+	
+	static GPT2Tokenizer tokenizer = GPT2Tokenizer.fromPretrained("");
+
+
+	
+	public static ArrayList<String> split_sentence(String sentence) {
+		String sentences[] = sentence.split(". ");
+		
+		List<Integer> totalTokens = new ArrayList<>();
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		
+		//getting the number of tokens for each sentence...
+		for(String sen : sentences) {
+			List<Integer> result = tokenizer.encode(sen);
+			totalTokens.add(result.size());
+			map.put(sen, result.size());
+		}
+		
+		ArrayList<String> chunks = new ArrayList<>();
+		
+		Integer tokenCount = 0;
+//		String chunk = "";
+		
+		for(Map.Entry<String, Integer> chunk : map.entrySet()) {
+			if(tokenCount > maxTokenSize) {
+				chunks.add(chunk.getKey());
+				tokenCount = 0;
+			}
+			tokenCount += chunk.getValue();
+		}
+		
+		return chunks;
+		
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		File csvFile = new File("website_text.csv");
+        FileWriter file = new FileWriter("scrappedData.json");
+
+		
+		Scanner sc = new Scanner(new File("website_text.csv"));
+		 	
+		sc.useDelimiter("\n");
+		
+		StringBuffer sb = new StringBuffer();
+		
+
+		ArrayList<String> splittedSentences = new ArrayList<String>();
+		List<Integer> result;
+		JSONObject jsonObject;
+		
+		Integer id = 1;
+		Integer noOfTokens = null;
+		String[] elements;
+		JSONArray dataJson = new JSONArray();
+		while(sc.hasNext()) {
+			String sentence = sc.next();
+			elements = sentence.split(" ");
+			if(elements.length <= 1) continue;
+			if(elements.length > maxTokenSize) {
+				splittedSentences = split_sentence(sentence);
+
+			}
+			
+			jsonObject = new JSONObject();
+			jsonObject.put("id", id++);
+			
+			jsonObject.put("sentence", sentence);
+			
+			dataJson.add(jsonObject);
+			
+			System.out.println(jsonObject);
+			
+		}
+		file.write(dataJson.toJSONString());
+		file.close();
+	}
+}
---
+++ src/main/java/com/salesken_scrapper/mavenproject/WebsiteTextToCSV.java
@@ -0,0 +1,119 @@
+package com.salesken_scrapper.mavenproject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class WebsiteTextToCSV {
+
+	static File csvFile = new File("website_text.csv");
+	
+	static FileWriter writer;
+	public static void initializeWriter() {
+	
+		try {
+			writer = new FileWriter(csvFile);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	static Set<String> map = new HashSet<>();
+
+	public static ArrayList<String> getData(String pageUrl) throws IOException {
+
+		if (map.contains(pageUrl.toLowerCase().trim()))
+			return null;
+
+		try {
+			
+			System.out.println("THIS IS URL: " + pageUrl);
+
+			map.add(pageUrl.toLowerCase().trim());
+
+			if (pageUrl.isEmpty())
+				return null;
+
+			Document doc = Jsoup.connect(pageUrl).get();
+
+			ArrayList<Elements> allElements = new ArrayList<>();
+
+//			allElements.add(doc.getElementsByAttribute("div"));
+			allElements.add(doc.getElementsByTag("h1"));
+			allElements.add(doc.getElementsByTag("h2"));
+			allElements.add(doc.getElementsByTag("h3"));
+			allElements.add(doc.getElementsByTag("h4"));
+			allElements.add(doc.getElementsByTag("h5"));
+			allElements.add(doc.getElementsByTag("h6"));
+			allElements.add(doc.getElementsByTag("p"));
+			allElements.add(doc.getElementsByTag("span"));
+
+			Elements links = doc.select("a");
+
+			for (Elements elements : allElements) {
+				for (Element el : elements) {
+					writer.append(el.text());
+					writer.append("\n");
+//		    		System.out.println(el.text());
+				}
+			}
+
+			ArrayList<String> urlList = new ArrayList<>();
+			System.out.println("Done writing homepage to csv.....");
+			for (Element link : links) {
+				if (link.attr("abs:href").contains("www.salesken.ai")
+						|| link.attr("abs:href").contains("//app.salesken.ai"))
+					urlList.add(link.attr("abs:href"));
+
+			}
+
+			return urlList;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void getUrls(ArrayList<String> urls) throws IOException {
+		if (urls.isEmpty())
+			return;
+
+		for (String url : urls) {
+			if (url != null) {
+				ArrayList<String> urlList = getData(url);
+				if (urlList != null) {
+					getUrls(urlList);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		initializeWriter();
+		
+		
+		String homeUrl = "https://www.salesken.ai/";
+
+		ArrayList<String> data = getData(homeUrl);
+
+		System.out.println("First step complete!!!!!");
+
+		Set<String> set = new HashSet<>(data);
+
+		ArrayList<String> urls = new ArrayList<>(set);
+
+		getUrls(urls);
+		
+		writer.close();
+	}
+}
